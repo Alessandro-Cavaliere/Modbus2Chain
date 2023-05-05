16d5458b5d1da158a9937f48ba925a67cb4c7029
Alessandro Cavaliere <alessandrocavaliere41@gmail.com>
2023-05-06 00:36:32 +0200

    beaglebone black connection to sensors

--- app/index.py
+++ app/index.py
@@ -1,45 +1,55 @@
-import socket
+import time
+import os
+from dotenv import load_dotenv, find_dotenv
+import paramiko
+import board
+import adafruit_dht
+from pymodbus.client import ModbusSerialClient as ModbusSerialClient
 
-from pymodbus.client import ModbusSerialClient as ModbusClient
-beaglebone_ip = "192.168.7.2" # Indirizzo IP del nostro BeagleBone Black
-beaglebone_port = 22 # Porta SSH del nostro BeagleBone Black
+load_dotenv(find_dotenv())
 
-# creazione socket TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Imposta i parametri per la connessione SSH - bbb = BeagleBone Black
+bbb_ip = os.getenv('BBB_IP')
+bbb_username = os.getenv('BBB_SSH_USERNAME')
+bbb_password = os.getenv('BBB_SSH_PASSWORD')
 
-
-# configurazione
-# Configura il client Modbus RTU
+# Configura il client Modbus RTU 
 client = ModbusClient(method='rtu', port='/dev/ttyS1', baudrate=9600, timeout=1, stopbits=1, bytesize=8, parity='N')
 
-# Connetti al bus Modbus
-connected = client.connect()
 
 try:
-    # connettiti al BeagleBone
-    s.connect((beaglebone_ip, beaglebone_port))
+    # Crea una connessione SSH
+    bbb = paramiko.SSHClient()
+    bbb.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    bbb.connect(beaglebone_ip, username=bbb_username, password=bbb_password)
+
+    # Esegui il comando per impostare la direzione della porta GPIO 45 come output
+    stdin, stdout, stderr = bbb.exec_command('echo out > /sys/class/gpio/gpio45/direction')
     print("Connessione riuscita!!!!")
-    # chiudi la connessione
+    # Sensor should be set to Adafruit_DHT.DHT11,
+    # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+    dht_device = adafruit_dht.DHT11(board.D18)
 
-    if connected:
-    # Leggi i registri dall'unità slave (ad es. Arduino)
-    # Supponendo che l'Arduino memorizzi la temperatura nel registro 0 e l'umidità nel registro 1
-        response = client.read_input_registers(address=0, count=2, unit=1)
-    
-    if response.isError():
-        print("Errore nella lettura dei registri")
+    temperature = dht_device.temperature
+    humidity = dht_device.humidity
+    print(
+    "Temp: {:.1f} , C Humidity: {}% ".format(
+    temperature, humidity
+    )
+    )
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
-            # Estrai la temperatura e l'umidità dai registri
-            temperature = response.registers[0] / 10
-            humidity = response.registers[1] / 10
-            print(f"Temperatura: {temperature} °C")
-            print(f"Umidità: {humidity} %")
-        
-            # Chiudi la connessione
-            client.close()
-            
+        print('Failed to get reading. Try again!')
+    # Fai suonare il buzzer per 10 secondi
+    for i in range(30):
+        bbb.exec_command(b"echo 1 > /sys/class/gpio/gpio45/value\n")
+        time.sleep(0.1)
+        bbb.exec_command(b"echo 0 > /sys/class/gpio/gpio45/value\n")
+        time.sleep(0.1)
+        print("SONO QUI")
 
-    s.close()
-except socket.error as e:
-    # gestisci il caso in cui la connessione fallisce
-    print("Connessione fallita: {e}")
+    # Chiudi la connessione
+    bbb.close()
+except Exception as e:
+    print(f"Errore: {e}")
