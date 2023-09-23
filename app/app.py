d98a606e3aa98d9c675b6fbcb08ef99e5174ad0d
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2023-09-23 20:16:50 +0200

    refactor: :recycle: Refactor and improve code structure

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import sys
-import subprocess
 import paramiko
 sys.path.insert(0, 'app\routes\auth.py')
 import auth
@@ -9,6 +8,7 @@ import datetime
 import mysql.connector
 from dotenv import load_dotenv
 import jwt
+import utils
 
 load_dotenv()
 app = Flask("Modbus2Chain")
@@ -23,7 +23,7 @@ connection = mysql.connector.connect(
 )
 
 # Chiave segreta per la firma del token JWT (dovrebbe essere segreta)
-jwt_secret_key = "Modbus"
+jwt_secret_key = os.getenv('SECRET_APP')
 
 
 bbb = paramiko.SSHClient()
@@ -31,32 +31,32 @@ bbb_ip = os.getenv('BBB_IP')
 bbb_username = os.getenv('BBB_SSH_USERNAME')
 bbb_password = os.getenv('BBB_SSH_PASSWORD')
 
+# SSH connection to the BBB
+bbb.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+bbb.connect(bbb_ip, username=bbb_username, password=bbb_password)
+utils.load_files_on_bbb(bbb)
+
 @app.route('/')
 def home():
     return "Welcome to Modbus2Chain Server"
-
-
-# Funzione per generare un token JWT
-def generate_jwt_token():
-    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)  # Scadenza di 30 minuti
-    payload = {
-        "exp": expiration_time,
-        "user" : "alex"
-    }
-    token = jwt.encode(payload, jwt_secret_key, algorithm="HS256")
-    return token
 
 
 # Route per il login
 @app.route('/login', methods=['POST'])
 def login():
     try:
-        # Connessione SSH alla BBB
-        bbb.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        bbb.connect(bbb_ip, username=bbb_username, password=bbb_password)
+        # Apply the authenticate_token_app middleware function here
+        if not auth.authenticate_token_app(request.headers.get('Authorization')):
+            return jsonify({"message": "Unauthorized"}), 401
+        
+        #restituzione utente: dao.login_user()
+        user={
+            "name":"alex",
+            "pass":"root"
+        }
 
         # Esegui l'autenticazione con successo e genera il token JWT
-        token = generate_jwt_token()
+        token = auth.generate_jwt_token(user)
 
         # Restituisci il token JWT in risposta
         return jsonify({"token": token}), 200
@@ -76,12 +76,24 @@ def login():
 
 @app.route('/sendData', methods=['POST'])
 def sendData():
-    python_command = 'python3 /var/lib/cloud9/Modbus2Chain/app/index.py'
+    try:
+        # Apply the authenticate_token_app middleware function here
+        if not auth.authenticate_token(request.headers.get('Authorization')):
+            return jsonify({"message": "Unauthorized"}), 401
+        
+        python_command = 'python3 /var/lib/cloud9/Modbus2Chain-master/master.py'
 
-    # Esegui il comando Python
-    stdin, stdout, stderr = bbb.exec_command(python_command)
-    print("Comando lanciato!!!!")
-    print(stdout.read().decode('utf-8'))
+        # Esegui il comando Python
+        stdin, stdout, stderr = bbb.exec_command(python_command)
+        print("Comando lanciato!!!!")
+        print(stdout.read().decode('utf-8'))   
+
+    except Exception as e:
+        # Gestisci eccezioni
+        print("Si è verificato un errore durante l'esecuzione del comando:", str(e))
+    
+    return "Comando completato con successo"  # Aggiungi una risposta HTTP appropriata
+
 
 @app.route('/protected', methods=['GET'])
 def protected_route():
@@ -90,4 +102,4 @@ def protected_route():
 
     return jsonify({"message": "Questa è una rotta protetta"}), 200
 
-app.run(host='192.168.178.21',port=8000)  #ip del pc locale
+app.run(host='192.168.10.174',port=8000)  #ip del pc locale
