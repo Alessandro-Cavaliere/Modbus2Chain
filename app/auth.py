d2685086405bff7441b7dc7b9acb70ba8d93f403
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2023-09-23 20:12:02 +0200

    refactor: :recycle: Refactoring of authentication file

--- app/auth.py
+++ app/auth.py
@@ -4,21 +4,17 @@ import datetime
 import os
 
 # Funzione middleware per l'autenticazione dell'app
-def authenticate_token_app():
-    auth_header = request.headers.get('Authorization')
-
+def authenticate_token_app(auth_header):
     if auth_header:
         token = auth_header.split(' ')[1]  # Estrae il token dal formato "Bearer <token>"
-
+        print(token)
         # Verifica il token (implementa la tua logica di verifica qui)
         if token == os.getenv('SECRET_APP'):
             return True
     return False
 
 # Funzione middleware per l'autenticazione
-def authenticate_token():
-    # Ottieni il token Bearer dall'header della richiesta
-    auth_header = request.headers.get('Authorization')
+def authenticate_token(auth_header):
     token = auth_header and auth_header.split(' ')[1]
 
     # Verifica se il token è presente
@@ -27,12 +23,7 @@ def authenticate_token():
 
     try:
         # Verifica e decodifica il token
-        user = jwt.decode(token, os.environ['JWT_SECRET'], algorithms=["HS256"])
-
-        # Se il token è valido, salva l'utente nella variabile globale
-        # oppure puoi anche passare l'utente nel contesto della richiesta (g)
-        # per utilizzarlo nelle rotte successive
-        request.user = user
+        jwt.decode(token, os.environ['JWT_SECRET'], algorithms=["HS256"])
         return True
     except jwt.ExpiredSignatureError:
         return False
@@ -40,9 +31,12 @@ def authenticate_token():
         return False
 
 # Funzione per generare un token JWT
-def generate_token(user):
-    print(datetime.date)
-    token = jwt.encode({"user": user, "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)},
-                       os.getenv('JWT_SECRET'), algorithm="HS256")
+def generate_jwt_token(user):
+    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)  # Scadenza di 30 minuti
+    payload = {
+        "exp": expiration_time,
+        "user" : user
+    }
+    token = jwt.encode(payload,os.getenv('JWT_SECRET'), algorithm="HS256")
     return token
 
