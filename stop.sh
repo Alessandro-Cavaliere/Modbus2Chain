06e2783b094576bd7ee7e67a7c97118aa6d83bee
Alessandro Cavaliere <alessandrocavaliere41@gmail.com>
2023-10-19 09:58:07 +0200

    feat: :sparkles: modified the stop.sh shutdown script to clean up the environment

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMPOSE_FILES="-f ./docker/docker-compose-org1.yaml -f ./docker/docker-compose-org2.yaml -f ./docker/docker-compose-orderer.yaml"
+COMPOSE_FILES="-f ./docker/docker-compose-org1.yaml -f ./docker/docker-compose-org2.yaml -f ./docker/docker-compose-orderer.yaml -f ./docker/docker-compose-ca-org1.yaml"
 IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} down --volumes --remove-orphans
 
 rm -rf ./crypto-material/*
